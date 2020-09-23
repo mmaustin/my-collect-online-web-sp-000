43ebca100d255ca221af99e1c9ddc5b319fe76ad
mccray austin <tubach75@hotmail.com>
2020-09-23 13:11:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(languages)
+  if block_given?
+    i = 0
 
-
+    collection = []
+    while i < languages.length
+      collection << yield(languages[i])
+      i += 1
+    end
+    collection
+  else
+    puts "There is no block given."
+  end
+end
+my_collect(["ruby", "javascript", "python", "objective-c"]) do |language|
+  language.upcase
+end
